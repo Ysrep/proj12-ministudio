327243e1b3ceba0bec00bf276b8c9478134c3a61
Loctryl <letallec.thibautl@gmail.com>
2022-03-30 10:06:36 +0200

    Update

--- src/app.js
+++ src/app.js
@@ -4,17 +4,13 @@ var dude;
 var scene;
 var cursor = [];
 
-
 class Dude extends Phaser.GameObjects.Image {
   constructor(scene, x, y) {
     super(scene, x, y, 'cara');
-
     this.startX = x;
     this.startY = y;
-
   }
   update() {
-
     if (cursor.up.isDown) {
       this.y -= 5;
     }
@@ -27,25 +23,17 @@ class Dude extends Phaser.GameObjects.Image {
     if (cursor.right.isDown) {
       this.x += 5;
     }
-
-
   }
 }
-
-
 
 class Zomb extends Phaser.GameObjects.Image {
   constructor(scene, x, y) {
     super(scene, x, y, 'zomb');
-
     this.startX = x;
     this.startY = y;
-
     this.speed = 0.01;
   }
-
   update() {
-
     this.x += (dude.x - this.x) * this.speed;
     this.y += (dude.y - this.y) * this.speed;
   }
@@ -77,51 +65,43 @@ class Example extends Phaser.Scene {
     var centerY = -100;
 
     var blocks = [];
+
+    var physicsContainer;
     scene = this;
     dude = this.add.existing(new Dude(this, 100, 100));
-
     for(let i=0; i < 5; i++){
       zombs.push(this.add.existing(new Zomb(this, Math.random() * 800, Math.random() * 500, dude)));
     }
 
+    physicsContainer = this.matter.add.gameObject(dude);
+    physicsContainer.setBounce(0.1);
+
     dude.setDepth(1);
     
-    for (var y = 0; y < mapHeight; y++)
-    {
-        for (var x = 0; x < mapWidth; x++)
-        {
-            var tx = (x - y) * tileWidthHalf;
-            var ty = (x + y) * tileHeightHalf;
-
-            var block = (x % 2 === 0) ? 'block-125' : 'block-132';
-
-            var tile = this.add.image(centerX + tx, centerY + ty, 'isoblocks', block);
-
-            tile.setData('row', x);
-            tile.setData('col', y);
-
-            tile.setDepth(0);
-
-            blocks.push(tile);
-        }
+    for (var y = 0; y < mapHeight; y++) {
+      for (var x = 0; x < mapWidth; x++) {
+        var tx = (x - y) * tileWidthHalf;
+        var ty = (x + y) * tileHeightHalf;
+        var block = (x % 2 === 0) ? 'block-125' : 'block-132';
+        var tile = this.add.image(centerX + tx, centerY + ty, 'isoblocks', block);
+        tile.setData('row', x);
+        tile.setData('col', y);
+        tile.setDepth(0);
+        blocks.push(tile);
+      }
     }
-
     this.cameras.main.zoom = 0.62;
     this.cameras.main.scrollX = -110;
-    for (let i = 0; i < 5; i++) {
-      zombs.push(this.add.existing(new Zomb(this, Math.random() * 800, Math.random() * 500, dude)));
-    }
     this.sound.pauseOnBlur = false;
-
     var music = this.sound.add('bread');
     cursor = this.input.keyboard.createCursorKeys()
     console.log(cursor);
-    music.play();
+    //music.play();
   }
 
   update() {
 
-    dude.update();
+    dude.update()
 
     zombs.forEach(function (zomb) {
       zomb.update();
@@ -134,16 +114,15 @@ class Example extends Phaser.Scene {
     }, this );
   }
 }
+
 class Example2 extends Phaser.Scene {
   constructor() {
     super({key:"Example2"});
   }
-
   create() {
     this.text = this.add.text(0,0,"Welcome", { font:"40px Impact"});
   }
 }
-
 
 const config = {
   type: Phaser.AUTO,
@@ -151,7 +130,14 @@ const config = {
   height: 768,
   backgroundColor: '#0d0d0d',
   parent: 'phase-example',
-  scene: [Example, Example2]
+  physics: {
+    default: 'matter',
+    matter: {
+      gravity : {y:0},
+      debug: true
+    }
+  },
+  scene: [Example, Example2], 
 };
 
 var game = new Phaser.Game(config);
