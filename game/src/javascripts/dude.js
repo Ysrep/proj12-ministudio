4614ce9a08dfa803c889d0633be13507e1383149
Falaron <45017124+Falaron@users.noreply.github.com>
2022-03-31 21:29:29 +0200

    dd

--- game/src/javascripts/dude.js
+++ game/src/javascripts/dude.js
@@ -3,22 +3,7 @@ import HealthBar from "./healthBar";
 
 class Dude extends Phaser.GameObjects.Sprite {
 
-    constructor (scene, color, x, y)
-    {
-        super(scene, x, y, "none");
-
-        this.color = color;
-
-        this.setTexture('dude');
-        this.setPosition(x, y);
-
-        scene.add.existing(this);
-
-        this.alive = true;
-
-        this.hp = new HealthBar(scene, x - 50, y - 110);
-
-    }
+    //code
 
 }
 
