bccec412096536d80e40089ffd1aa4656bd4ad79
Loctryl <letallec.thibautl@gmail.com>
2022-04-01 09:00:59 +0200

    Update

--- src/laser.js
+++ src/laser.js
@@ -1,22 +1,18 @@
 class Laser extends Phaser.Physics.Arcade.Sprite {
 	constructor(scene, x, y) {
-		super(scene, x, y, 'laser');
-        
+		super(scene, x, y, 'laser');   
 	}
-
-    fire(x, y, Yangle, Xangle) {
-		this.body.reset(x, y);
+  
+  fire(x, y, Yangle, Xangle) {
+	  this.body.reset(x, y);
 		this.setActive(true);
 		this.setVisible(true);
  
 		this.setVelocityY(Yangle*3);
-        this.setVelocityX(Xangle*3);
-        
-
-        
+    this.setVelocityX(Xangle*3);    
 	}
-
-    preUpdate(time, delta) {
+  
+  preUpdate(time, delta) {
 		super.preUpdate(time, delta);
  
 		if (this.y <= 0) {
